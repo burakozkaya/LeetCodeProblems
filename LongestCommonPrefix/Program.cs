cf9b4f32804854c4404045e0fb73c2b022c55c90
Burak Özkaya <ozkayaburak97@gmail.com>
2023-10-17 10:31:21 +0300

    Add project files.

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,32 @@
+﻿using static LongestCommonPrefix.Program;
+
+namespace LongestCommonPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string[] input = new string[] { "flower", "flour", "floppy" };
+            string result = solution.LongestCommonPrefix(input);
+            Console.WriteLine(result);
+        }
+        public class Solution
+        {
+            public string LongestCommonPrefix(string[] strs)
+            {
+                string prefix = strs[0];
+                for (var i = 1; i < strs.Length; i++)
+                {
+                    while (strs[i].IndexOf(prefix) != 0)
+                    {
+                        prefix = prefix.Substring(0, prefix.Length - 1);
+                        if (prefix.Length == 0)
+                            return string.Empty;
+                    }
+                }
+                return prefix;
+            }
+        }
+    }
+}
